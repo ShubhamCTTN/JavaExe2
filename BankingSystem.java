f990c4ebd8dc70df28c7acbc7750e6ca932b4c2f
ShubhamCTTN <114070622+ShubhamCTTN@users.noreply.github.com>
2022-09-28 10:26:04 +0530

    Add files via upload

---
+++ BankingSystem.java
@@ -0,0 +1,41 @@
+package exe2;
+
+public class BankingSystem {
+    private Double rateOfintrest=1.2;
+    private String pName="Ravi Gupta";
+    private int accountNo=50100235;
+    private int salary=32000;
+public BankingSystem() {}
+    public BankingSystem(double rateOfintrest, String pName,int accountNo, int salary) {
+        this.rateOfintrest = rateOfintrest;
+        this.pName = pName;
+        this.accountNo = accountNo;
+        this.salary = salary;
+    }
+    
+    public double getRateOfintrest() {
+        return rateOfintrest;
+    }
+    public void setRateOfintrest(double rateOfintrest) {
+        this.rateOfintrest = rateOfintrest;
+    }
+    public String getName() {
+        return pName;
+    }
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+    public int getAccountNo() {
+        return accountNo;
+    }
+    public void setAccountNo(Integer accountNo) {
+        this.accountNo = accountNo;
+    }
+    public int getsalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+}
